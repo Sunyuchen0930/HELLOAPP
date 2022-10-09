3e71277854a08f445e3bbe489144f7907bf88a80
syc <2582410772@qq.com>
2022-10-09 16:05:17 +0800

    new class hello

---
+++ helloapp/src/edu.bupt.hello/hello.java
@@ -0,0 +1,28 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hihi");
+        System.out.println("hihi555");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
